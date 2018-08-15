ec40903ac8e12541fb4384687f43fb7694e5bf26
Alex Didenko <alex.didenko753@gmail.com>
2018-08-15 12:10:54 +0300

    added assets

---
+++ public/js/admin.js
@@ -0,0 +1,198 @@
+function initEditor() {
+    console.log('mce start');
+    tinymce.init({
+        selector: '.ckeditor',
+        plugins: "image, link, code",
+        toolbar: "link image code",
+        language: 'ru',
+        file_browser_callback : elFinderBrowser,
+        init_instance_callback: function (editor) {
+            editor.on('blur', function (e) {
+                editor.save();
+                multiItemFill($('#' + editor.id).data('target'));
+
+                var matches;
+                if (typeof app != 'undefined' && (matches = /block-(\d+)/ig.exec(editor.id))) {
+                    app.__vue__.$refs.contentBuilder.blocks[matches[1]].text = editor.getContent();
+                }
+            });
+        }
+    });
+}
+
+initEditor();
+
+function stopEditor() {
+    console.log('mce stopped');
+    tinymce.remove();
+}
+
+function restartEditor() {
+    stopEditor();
+    initEditor();
+}
+
+function elFinderBrowser (field_name, url, type, win) {
+    tinymce.activeEditor.windowManager.open({
+        file: appConfig.elfinderRoute,
+        title: 'elFinder 2.0',
+        width: 900,
+        height: 450,
+        resizable: 'yes'
+}, {
+        setUrl: function (url) {
+            win.document.getElementById(field_name).value = url;
+        }
+    });
+    return false;
+}
+
+function processSelectedFile(e, t) {
+    $("#" + t).val(e).change();
+
+    var matches;
+    if (typeof app != 'undefined' && (matches = /block-(\d+)-image-(\d+)/ig.exec(t))) {
+        app.__vue__.$refs.contentBuilder.blocks[matches[1]].images[matches[2]].src = e;
+    }
+}
+
+function multiItemFill(id) {
+    var multiItems = [];
+    $('#' + id).parent().find('.multi-item-container .multi-item').each(function(index) {
+        var subItems = {};
+        $(this).find('[data-target]').each(function() {
+            subItems[$(this).data('key')] = $(this).val();
+            subItems['sort'] = index;
+        });
+        multiItems.push(subItems);
+    });
+
+    $('#' + id).val(JSON.stringify(multiItems));
+    console.log(JSON.stringify(multiItems));
+}
+
+jQuery(function($) {
+
+    feather.replace();
+
+    $(document).on("click", ".popup_selector", function (e) {
+        e.preventDefault();
+        var t = $(this).attr("data-inputid");
+        var n = "/elfinder/popup/";
+        var r = n + t;
+        $.colorbox({href: r, fastIframe: true, iframe: true, width: "70%", height: "50%"})
+    });
+
+    $(".popup_content").colorbox({inline:true, href: $(this).attr("href"), width: "70%", height: "50%"});
+
+    $(document).on('change', '.img-field', function() {
+        var previewImgField = $(this).val() ? '<img src="/' + $(this).val() + '" height="37px">' : '',
+            target = '.img-preview[data-preview-id="' + $(this).attr('id') + '"]';
+        $(target).html(previewImgField);
+    });
+    $('.img-field').change();
+
+
+    $('.multi-item-container').on('keyup, change', '.multi-item-element', function(e) {
+        multiItemFill($(this).data('target'));
+    });
+
+    $('.multi-item-container').on('click', '.plus', function(e) {
+        e.preventDefault();
+
+        var item = $(this).closest('.multi-item'),
+            editor = item.find('.ckeditor');
+
+        if (editor.length)
+            stopEditor();
+
+        var newEl = item.clone(),
+            imgField = newEl.find(".img-field"),
+            fileField = newEl.find(".file-field"),
+            previewImgContainer = newEl.find(".img-preview"),
+            uploadButton = newEl.find('[data-inputid]');
+
+        // targets for images
+        if (imgField.length) {
+            var newId = imgField.attr('id').replace(/[0-9]+$/g, Math.floor(Date.now() / 1000));
+
+            imgField.attr('id', newId);
+            previewImgContainer.attr('data-preview-id', newId);
+            uploadButton.attr('data-inputid', newId);
+
+            previewImgContainer.html('');
+        }
+        // targets for files
+        if (fileField.length) {
+            var newId = fileField.attr('id').replace(/[0-9]+$/g, Math.floor(Date.now() / 1000));
+
+            fileField.attr('id', newId);
+            uploadButton.attr('data-inputid', newId);
+        }
+
+        newEl.find('input, textarea').each(function() {
+            $(this).val('');
+        });
+
+        item.after(newEl);
+        multiItemFill(item.find('[data-target]').data('target'));
+
+        if (editor.length)
+            initEditor();
+    });
+
+    $('.multi-item-container').on('click', '.minus', function(e) {
+        e.preventDefault();
+        var item = $(this).closest('.multi-item'),
+            container = $(this).closest('.multi-item-container'),
+            target = item.find('[data-target]').data('target');
+        if (container.find('.multi-item').length > 1) {
+            item.remove();
+            multiItemFill(target);
+        }
+    });
+
+    $('.multi-item-container').on('click', '.up', function(e) {
+        e.preventDefault();
+        var item = $(this).closest('.multi-item'),
+            prev = item.prev(),
+            editor = item.find('.ckeditor');
+
+        if (prev) {
+            if (editor.length)
+                stopEditor();
+
+            item.insertBefore(prev);
+
+            if (editor.length)
+                initEditor();
+
+            multiItemFill(item.find('[data-target]').data('target'));
+        }
+    });
+
+    $('.multi-item-container').on('click', '.down', function(e) {
+        e.preventDefault();
+        var item = $(this).closest('.multi-item'),
+            next = item.next(),
+            editor = item.find('.ckeditor');
+
+        if (next) {
+            if (editor.length)
+                stopEditor();
+
+            item.insertAfter(next);
+
+            if (editor.length)
+                initEditor();
+
+            multiItemFill(item.find('[data-target]').data('target'));
+        }
+
+    });
+
+    $('.sidebar-sticky .nav-link').each(function() {
+        if (location.pathname.indexOf($(this).attr('href')) > -1)
+            $(this).addClass('active');
+    });
+});
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <base href="/">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="//fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="/packages/colorbox/colorbox.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div class="sidebar-sticky">
+
+                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>Контент</span>
+                        </h6>
+                        <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/page">
+                                    <span data-feather="book-open"></span>
+                                    Страницы
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/news">
+                                    <span data-feather="align-left"></span>
+                                    Новости
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/portfolio">
+                                    <span data-feather="shopping-cart"></span>
+                                    Портфолио
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/catalog_stone">
+                                    <span data-feather="shopping-cart"></span>
+                                    Товары каталога камня
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/catalog_product">
+                                    <span data-feather="shopping-cart"></span>
+                                    Товары каталога изделий
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>Администрирование</span>
+                        </h6>
+                        <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/menu">
+                                    <span data-feather="menu"></span>
+                                    Меню
+                                </a>
+                                <a class="nav-link" href="/admin/user">
+                                    <span data-feather="users"></span>
+                                    Пользователи
+                                </a>
+                                <a class="nav-link" href="/admin/template">
+                                    <span data-feather="sidebar"></span>
+                                    Шаблоны
+                                </a>
+                                <a class="nav-link" href="/admin/log">
+                                    <span data-feather="eye"></span>
+                                    Лог изменений
+                                </a>
+                                <a class="nav-link" href="{{-- route('settings', [], false) --}}">
+                                    <span data-feather="settings"></span>
+                                    Настройки
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>{{ Auth::user()->name }}</span>
+                        </h6>
+                        <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    <span data-feather="log-out"></span>
+                                    Выйти</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+
+
+                    </div>
+                </nav>
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                    <div class="container align-items-center pb-2 mb-3">
+                        @yield('content')
+                    </div>
+                </main>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script>
+        var appConfig = {
+            elfinderRoute: '<? /*route('elfinder.tinymce4')*/ ?>'
+        };
+    </script>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src='/packages/tinymce/tinymce.min.js'></script>
+    <script src='/packages/colorbox/jquery.colorbox-min.js'></script>
+    <script src="//unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script src="{{ asset('js/admin.js') }}"></script>
+
+</body>
+</html>
