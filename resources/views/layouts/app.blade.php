e6e99ffb5757d022f3d5c090a8278cdca48eec67
Alex Didenko <alex.didenko753@gmail.com>
2018-09-13 17:56:49 +0300

    fix base hfer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <base href="http://altair.test/">
+    <base href="/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="description" content="" />
