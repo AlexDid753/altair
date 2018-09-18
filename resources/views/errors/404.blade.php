c0840c55fd4ad900172bd2e2bfe2080c3f427c63
Alex Didenko <alex.didenko753@gmail.com>
2018-09-18 15:52:49 +0300

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <section id="content">
+        <div class="content-page">
+            <div class="container">
+                <div class="content-about center">
+                    <h2 class="title30 play-font text-uppercase dark font-bold">Страница не найдена</h2>
+                    <p class="desc"><a href="/" class="shop-button bg-white">Перейти на главную</a></p>
+                    <div class="white-space"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
