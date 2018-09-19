be7a4d2120087957cef915e34fd255c94cb619d7
Alex Didenko <alex.didenko753@gmail.com>
2018-09-19 18:29:37 +0300

    update index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="wrap-item" data-navigation="true" data-transition="fade" data-itemscustom="[[0,1]]">
                     <div class="item-slider item-slider2">
                         <div class="banner-thumb">
-                            <a href="{{$model->href1}}"><img src="{{$model->image1}}" alt="" /></a>
+                            <a href="{{$model->href1}}"><img src="{{resize($model->image1,1170,560)}}" alt="" /></a>
                         </div>
                         <div class="banner-info animated text-center" data-animated="zoomIn">
                             <h2 class="title48 play-font font-normal text-uppercase dark">{{$model->text_img1}}</h2>
@@ -41,14 +41,14 @@
                 <div class="row">
                     <div class="col-sm-6 col-xs-12">
                         <div class="item-banner-jewelry3 zoom-rotate fade-out-in">
-                            <a href="{{$model->href2}}" class="adv-thumb-link"><img src="{{$model->image2}}" alt="" />
+                            <a href="{{$model->href2}}" class="adv-thumb-link"><img src="{{resize($model->image2, 575,383)}}" alt="" />
                                 <div class="banner-info">
                                     <h3 class="title30 play-font dark font-italic">{{$model->text_img2}}</h3>
                                 </div>
                             </a>
                         </div>
                         <div class="item-banner-jewelry3 zoom-image line-scale style2">
-                            <a href="{{$model->href3}}" class="adv-thumb-link"><img src="{{$model->image3}}" alt="" />
+                            <a href="{{$model->href3}}" class="adv-thumb-link"><img src="{{resize($model->image3, 575,383)}}" alt="" />
                                 <div class="banner-info">
                                     <h3 class="title30 play-font dark font-italic">{{$model->text_img3}}</h3>
                                         <h3 class="title18 play-font dark">Upto 50% off</h3>
@@ -58,7 +58,7 @@
                     </div>
                     <div class="col-sm-6 col-xs-12">
                         <div class="item-banner-jewelry3 zoom-image line-scale style2">
-                            <a href="{{$model->href4}}" class="adv-thumb-link"><img src="{{$model->image4}}" alt="" />
+                            <a href="{{$model->href4}}" class="adv-thumb-link"><img src="{{resize($model->image4, 575,383)}}" alt="" />
                                 <div class="banner-info">
                                     <h3 class="title30 play-font dark font-italic">{{$model->text_img4}}</h3>
                                         <h3 class="title18 play-font dark">Upto 80% off</h3>
@@ -66,7 +66,7 @@
                             </a>
                         </div>
                         <div class="item-banner-jewelry3 zoom-rotate fade-out-in">
-                            <a href="{{$model->href5}}" class="adv-thumb-link"><img src="{{$model->image5}}" alt="" />
+                            <a href="{{$model->href5}}" class="adv-thumb-link"><img src="{{resize($model->image5, 575,383)}}" alt="" />
                                 <div class="banner-info">
                                     <h3 class="title30 play-font dark font-italic">{{$model->text_img5}}</h3>
                                         <h8 class="title18 play-font dark">$290.00 - $675.00</h8>
