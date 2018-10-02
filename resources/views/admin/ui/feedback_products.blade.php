31d7ec034532b883feb1585db5f37f4220d61edc
Alex Didenko <alex.didenko753@gmail.com>
2018-10-02 10:46:48 +0300

    fix feedback with trashed + ru.json

--- resources/views/admin/ui/feedback_products.blade.php
+++ resources/views/admin/ui/feedback_products.blade.php
@@ -1,23 +1,27 @@
 <?php  $data = isset($fieldValue['value']) ? $fieldValue['value'] : json_decode($model->$fieldName);   ?>
 @if (!empty($data))
-<div class="form-group row">
+    <div class="form-group row">
 
-    <label class="col-md-12 col-form-label">Избранные товары</label>
+        <label class="col-md-12 col-form-label">Выбранные товары</label>
 
-    <div class="col-md-12">
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th scope="col">Товар</th>
-            </tr>
-            </thead>
-            @foreach($data as $data_item)
-                <?php $product = \App\Product::find($data_item->id); ?>
+        <div class="col-md-12">
+            <table class="table table-bordered">
+                <thead>
                 <tr>
-                    <th><a href="{{$product->url}}">{{$product->title}}</a></th>
+                    <th scope="col">Товар</th>
                 </tr>
-            @endforeach
-        </table>
+                </thead>
+                @foreach($data as $data_item)
+                    <?php $product = \App\Product::withTrashed()->find($data_item->id); ?>
+                    <tr>
+                        <th><a href="{{$product->url}}">{{$product->title}}</a>
+                            <?php if ($product->trashed()) {
+                                echo ' (Продукт удален)';
+                            } ?>
+                        </th>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
-</div>
 @endif
