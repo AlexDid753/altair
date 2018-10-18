e9b1c16b5318c47528e6fc8dd975d08c2edef3bf
Alex Didenko <alex.didenko753@gmail.com>
2018-10-18 17:55:38 +0300

    editing main sliders

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,20 @@
 <!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <?php
-    if (!isset($model)) {
-        $model = new \App\Page();
-        $model->name = config('app.name');
-    }
+if (!isset($model)) {
+    $model = new \App\Page();
+    $model->name = config('app.name');
+}
 ?>
 <head>
     <base href="/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="robots" content="" />
-    <meta name="yandex-verification" content="c4d052ac7add82f5" />
-    <meta name="google-site-verification" content="2TyHFP-Ix37YtnO3yNrS550AJY95TSk8Os49fXPeON4" />
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <meta name="robots" content=""/>
+    <meta name="yandex-verification" content="c4d052ac7add82f5"/>
+    <meta name="google-site-verification" content="2TyHFP-Ix37YtnO3yNrS550AJY95TSk8Os49fXPeON4"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $model->meta_title ?: $model->name }}</title>
     @if ($model->meta_description)
@@ -41,6 +41,38 @@
     <link rel="stylesheet" type="text/css" href="/css/responsive.css" media="all"/>
     <link rel="stylesheet" type="text/css" href="/css/browser.css" media="all"/>
     <!-- <link rel="stylesheet" type="text/css" href="/css/rtl.css" media="all"/> -->
+    <style>
+        @if (isset($model->slider_linked_images) && count($model->slider_linked_images))
+            @foreach ($model->slider_linked_images as $slider_item)
+                @media (min-width: 768px) {
+                    .item-slider .banner-info[data-number='{{$slider_item->sort}}'] {
+                        @switch($slider_item->position)
+                            @case('right')
+                                @break
+
+                            @case('left')
+                                left: auto;
+                                @break
+
+                            @case('center')
+                                right:0;
+                                @break
+
+                            @default
+                        @endswitch
+                        {{isset($slider_item->position_top)&&!empty($slider_item->position_top) ?
+                        'bottom: inherit;
+                        top:'.$slider_item->position_top.'px;' : ""}}
+                    }
+
+                    .item-slider .banner-info[data-number='{{$slider_item->sort}}'] h2 {
+                        font-weight: {{isset($slider_item->font_weight) ? $slider_item->font_weight : ''}};
+                    }
+                }
+            @endforeach
+        @endif
+
+    </style>
 </head>
 <body class="preload" style="background:#f4f4f4">
 <div class="wrap">
