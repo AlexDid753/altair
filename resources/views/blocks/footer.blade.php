0d00997a1bf25fa7e92901025a38eea37057fc55
Alex Didenko <alex.didenko753@gmail.com>
2018-11-08 15:13:45 +0300

    address to footer

--- resources/views/blocks/footer.blade.php
+++ resources/views/blocks/footer.blade.php
@@ -10,7 +10,8 @@
                                 <div class="thanks">
                                     <p class="desc white opaci thanks">Спасибо за Вашу подписку.</p>
                                 </div>
-                                <p class="desc white opaci">Введите свою почту чтобы подписаться на рассылку о новинках.</p>
+                                <p class="desc white opaci">Введите свою почту чтобы подписаться на рассылку о
+                                    новинках.</p>
                                 <form class="form-newsletter2 feedback-form" method="post">
                                     <input type="hidden" name="type" value="subscribe">
                                     <input required
@@ -29,10 +30,12 @@
                             <div class="block-footer2">
                                 <div class="logo logo-footer2 text-center">
                                     <a href="/">
-                                        <span class="logo-icon logo-icon_big title24 round white"><img src="{{$settings->logo}}" alt=""></span>
+                                        <span class="logo-icon logo-icon_big title24 round white"><img
+                                                    src="{{$settings->logo}}" alt=""></span>
                                     </a>
                                 </div>
-                                <h2 class="title18 play-font white text-center font-italic">Подпишитесь на нас в соцсетях</h2>
+                                <h2 class="title18 play-font white text-center font-italic">Подпишитесь на нас в
+                                    соцсетях</h2>
                                 <div class="social-network-footer text-center">
                                     <a href="#" class="inline-block round"><i class="fa fa-vk"></i></a>
                                     <a href="#" class="inline-block round"><i class="fa fa-instagram"></i></a>
@@ -47,11 +50,18 @@
                                 <ul class="list-none contact-foter2">
                                     <li>
                                         <i class="fa fa-volume-control-phone white"></i>
-                                        <span class="text-uppercase white opaci"><a class="white opaci" href="tel:{{$settings->phone}}">{{$settings->phone}}</a></span>
+                                        <span class="text-uppercase white opaci"><a class="white opaci"
+                                                                                    href="tel:{{$settings->phone}}">{{$settings->phone}}</a></span>
                                     </li>
                                     <li>
                                         <i class="fa fa-envelope white"></i>
-                                        <a class="white opaci" href="mailto:{{$settings->email}}">{{$settings->email}}</a>
+                                        <a class="white opaci"
+                                           href="mailto:{{$settings->email}}">{{$settings->email}}</a>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-home white" aria-hidden="true"></i>
+                                        <span class="white opaci"><a class="white opaci"
+                                                                                    href="tel:{{$settings->address}}">{{$settings->address}}</a></span>
                                     </li>
                                 </ul>
                             </div>
@@ -65,7 +75,8 @@
                             <li><a href="{{$item->getUrl()}}" class="white wobble-top">{{$item->name}}</a></li>
                         @endforeach
                     </ul>
-                    <p class="desc copyright-footer text-center"><span class="white opaci">Альтаир © 2018 Создание сайта — </span> <a target="_blank" href="https://Molinos.Ru" class="white">Molinos.Ru</a></p>
+                    <p class="desc copyright-footer text-center"><span class="white opaci">Альтаир © 2018 Создание сайта — </span>
+                        <a target="_blank" href="https://Molinos.Ru" class="white">Molinos.Ru</a></p>
                 </div>
             </div>
         </div>
