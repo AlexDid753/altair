316ad74796fbaa12a587a50fa7ecd495cf2fbf66
Alex Didenko <alex.didenko753@gmail.com>
2018-11-19 10:26:50 +0300

    transliterate name of products

--- public/js/theme.js
+++ public/js/theme.js
@@ -402,6 +402,24 @@
             return parseInt(str.replace(/\D+/g,"").slice(1));
         }
 
+        //Если с английского на русский, то передаём вторым параметром true.
+        let transliterate = (
+            function() {
+                var
+                    rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
+                    eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g)
+                ;
+                return function(text, engToRus) {
+                    var x;
+                    for(x = 0; x < rus.length; x++) {
+                        text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
+                        text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());
+                    }
+                    return text;
+                }
+            }
+        )();
+
 
         //Форма обратной связи
         $('form.feedback-form').submit(function (event) {
@@ -490,7 +508,7 @@
                 let name = $(this).find('.product-name a').text(),
                  price = parseInt($(this).find('.product-price .amount').text()),
                  sum = (payments_type == 2) ? price * 0.5 : price,
-                 cart_item_data = `sum:${sum}.0,tax:none,tax_sum:0.0,name:${name},price:${price}.0,quantity:1.0;`;
+                 cart_item_data = `sum:${sum}.0,tax:none,tax_sum:0.0,name:${transliterate(name)},price:${price}.0,quantity:1.0;`;
                 str = str + cart_item_data;
             });
             str = removeLastSym(str);
