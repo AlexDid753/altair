5fc76b72c3a061ff4a1cda32863efed74b095c81
Alex Didenko <alex.didenko753@gmail.com>
2019-01-28 17:30:11 +0300

    format

--- resources/views/blocks/header.blade.php
+++ resources/views/blocks/header.blade.php
@@ -1,56 +1,56 @@
 <header id="header">
-    <div class="nav-header nav-header-jewelry5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="logo logo1">
-                        <a href="/">
-                            <span class="logo-icon logo-icon_big title24 round white"><img src="{{$settings->logo}}" alt=""></span>
-                        </a>
-                    </div>
-                    <!-- End logo -->
-                    <nav class="main-nav main-nav1 pull-left dark-style">
-                        <ul>
-                            @foreach($topMenu as $menuItem)
-                                <li class="{{ count($menuItem->childrens) ? 'menu-item-has-children' : ''}}">
-                                    <a href="{{$menuItem->getUrl()}}">{{$menuItem->name}}</a>
-                                    @if (count($menuItem->childrens)) {{--todo сделать рекурсивное применение шаблона --}}
-                                        <ul class="sub-menu">
-                                            @foreach($menuItem->childrens as $subMenuItem)
-                                                <li class="{{ count($subMenuItem->childrens) ? 'menu-item-has-children' : ''}}"><a href="{{ $subMenuItem->getUrl() }}">{{ $subMenuItem->name }}</a>
-                                                    @if (count($subMenuItem->childrens))
-                                                        <ul class="sub-menu">
-                                                            @foreach($subMenuItem->childrens as $subMenuItem)
-                                                                <li><a href="{{ $subMenuItem->getUrl() }}">{{ $subMenuItem->name }}</a></li> {{--todo getUrl требует оптимизации, сохранения url на сохранение menu --}}
-                                                            @endforeach
-                                                        </ul>
-                                                    @endif
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    @endif
-                                </li>
+  <div class="nav-header nav-header-jewelry5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="logo logo1">
+            <a href="/">
+              <span class="logo-icon logo-icon_big title24 round white"><img src="{{$settings->logo}}" alt=""></span>
+            </a>
+          </div>
+          <!-- End logo -->
+          <nav class="main-nav main-nav1 pull-left dark-style">
+            <ul>
+              @foreach($topMenu as $menuItem)
+                <li class="{{ count($menuItem->childrens) ? 'menu-item-has-children' : ''}}">
+                  <a href="{{$menuItem->getUrl()}}">{{$menuItem->name}}</a>
+                  @if (count($menuItem->childrens)) {{--todo сделать рекурсивное применение шаблона --}}
+                  <ul class="sub-menu">
+                    @foreach($menuItem->childrens as $subMenuItem)
+                      <li class="{{ count($subMenuItem->childrens) ? 'menu-item-has-children' : ''}}"><a href="{{ $subMenuItem->getUrl() }}">{{ $subMenuItem->name }}</a>
+                        @if (count($subMenuItem->childrens))
+                          <ul class="sub-menu">
+                            @foreach($subMenuItem->childrens as $subMenuItem)
+                              <li><a href="{{ $subMenuItem->getUrl() }}">{{ $subMenuItem->name }}</a></li> {{--todo getUrl требует оптимизации, сохранения url на сохранение menu --}}
                             @endforeach
-                        </ul>
-                        <a href="#" class="toggle-mobile-menu"><span></span></a>
-                    </nav>
-                    <div class="logo logo2">
-                        <a href="/">
-                            <span class="logo-icon logo-icon_big title24 round white"><img src="{{$settings->logo}}" alt=""></span>
-                        </a>
-                    </div>
-                    <!-- End Main Nav -->
-                    <div class="cart-top2-phone"><i class="fa fa-volume-control-phone white"></i>
-                        <a href="tel:{{$settings->phone}}">{{$settings->phone}}</a></div>
+                          </ul>
+                        @endif
+                      </li>
+                    @endforeach
+                  </ul>
+                  @endif
+                </li>
+              @endforeach
+            </ul>
+            <a href="#" class="toggle-mobile-menu"><span></span></a>
+          </nav>
+          <div class="logo logo2">
+            <a href="/">
+              <span class="logo-icon logo-icon_big title24 round white"><img src="{{$settings->logo}}" alt=""></span>
+            </a>
+          </div>
+          <!-- End Main Nav -->
+          <div class="cart-top2-phone"><i class="fa fa-volume-control-phone white"></i>
+            <a href="tel:{{$settings->phone}}">{{$settings->phone}}</a></div>
 
-                    <ul class="wrap-cart-top2 list-inline-block pull-right">
-                        <li>
-                            <a href="{{\App\Page::find(2)->url}}" class="title18 wishlist-link" title="Корзина"><span class="white"><i class="fa fa-shopping-basket"></i></span>
-                                <sup class="title10 round dark bg-white">{{count(\App\Product::liked())}}</sup></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+          <ul class="wrap-cart-top2 list-inline-block pull-right">
+            <li>
+              <a href="{{\App\Page::find(2)->url}}" class="title18 wishlist-link" title="Корзина"><span class="white"><i class="fa fa-shopping-basket"></i></span>
+                <sup class="title10 round dark bg-white">{{count(\App\Product::liked())}}</sup></a>
+            </li>
+          </ul>
         </div>
+      </div>
     </div>
+  </div>
 </header>
