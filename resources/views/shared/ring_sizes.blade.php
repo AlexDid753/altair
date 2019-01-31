8f95dce729c52f854ca7c5c3677658e669568ef7
Alex Didenko <alex.didenko753@gmail.com>
2019-01-31 15:13:05 +0300

    little fixes

--- resources/views/shared/ring_sizes.blade.php
+++ resources/views/shared/ring_sizes.blade.php
@@ -1,6 +1,6 @@
 <ul class="list-inline-block list-attr-label">
   @foreach (Product::excepted_sizes_dropdown() as $key => $size)
-    @if (!in_array($key, $model->excepted_sizes))
+    @if (!in_array($key, $model->excepted_sizes ?? []))
       <li><span data-id="{{$model->id}}">{{number_format( (float) $size, 1, '.', '')}}</span></li>
     @endif
   @endforeach
