3411076da5dc2be374ea30ea3a4540e46d1698f7
Alex Didenko <alex.didenko753@gmail.com>
2019-03-05 16:42:08 +0300

    cart data in email

--- resources/views/admin/ui/feedback_products.blade.php
+++ resources/views/admin/ui/feedback_products.blade.php
@@ -1,31 +1,4 @@
 <?php  $data = isset($fieldValue['value']) ? $fieldValue['value'] : json_decode($model->$fieldName);   ?>
 @if (!empty($data))
-  <div class="form-group row">
-    <label class="col-md-12 col-form-label">Выбранные товары</label>
-    <div class="col-md-12">
-      <table class="table table-bordered">
-        <thead>
-        <tr>
-          <th scope="col">Товар</th><th scope="col">Информация</th>
-        </tr>
-        </thead>
-        @foreach($data as $data_item)
-              <?php $product = \App\Product::withTrashed()->find($data_item->id); ?>
-          <tr>
-            <th>
-              <a href="{{$product->url}}">{{$product->title}}</a>
-                <?php if ($product->trashed()) {
-                    echo ' (Продукт удален)';
-                } ?>
-            </th>
-            <th>
-              @if(!empty($data_item->size))
-                {{'Размер: '.$data_item->size}}
-              @endif
-            </th>
-          </tr>
-        @endforeach
-      </table>
-    </div>
-  </div>
+  @include('admin.ui.products_info_table')
 @endif
---
+++ resources/views/admin/ui/products_info_table.blade.php
@@ -0,0 +1,30 @@
+<div class="form-group row">
+  <label class="col-md-12 col-form-label">Выбранные товары</label>
+  <div class="col-md-12">
+    <table class="table table-bordered">
+      <thead>
+      <tr>
+        <th scope="col">#</th><th scope="col">Товар</th><th scope="col">Информация</th>
+      </tr>
+      </thead>
+      @foreach($data as $data_item)
+            <?php $product = Product::withTrashed()->find($data_item->id); ?>
+        <tr>
+          <th>
+            <a href="{{$product->url}}">
+              <img src="{{resize($product->preview_image(), 100, 100)}}" alt=""/>
+            </a>
+          </th>
+          <th>
+            <a href="{{$product->url}}">{{$product->title}}</a>
+            {{($product->trashed())?' (Продукт удален)' : ''}}
+          </th>
+          <th>
+            <p>Цена: {{$product->price}}</p>
+            <p>{{($data_item->size)?'Размер: '.$data_item->size : ''}}</p>
+          </th>
+        </tr>
+      @endforeach
+    </table>
+  </div>
+</div>
