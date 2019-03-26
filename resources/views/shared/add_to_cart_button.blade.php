7b20c282aebfc3101551fe0069c0f154f300fda1
LanaYer <e.svetlana@molinos.ru>
2019-03-26 14:09:49 +0300

    reach goals

--- resources/views/shared/add_to_cart_button.blade.php
+++ resources/views/shared/add_to_cart_button.blade.php
@@ -1,1 +1,4 @@
-<a href="#" class="wishlist-link black inline-block"><i class="icon {{ $model->isLiked() ? 'ion-ios-cart' : 'ion-ios-cart-outline' }}" data-slug="{{$model->slug}}"></i><span class="title10 white text-uppercase">{{ $model->isLiked() ? 'Удалить из корзины' : 'Добавить в корзину' }}</span></a>
+<a href="#" class="wishlist-link black inline-block" onclick="ym(50521729, 'reachGoal', 'korzina'); return true;">
+    <i class="icon {{ $model->isLiked() ? 'ion-ios-cart' : 'ion-ios-cart-outline' }}" data-slug="{{$model->slug}}"></i>
+    <span class="title10 white text-uppercase">{{ $model->isLiked() ? 'Удалить из корзины' : 'Добавить в корзину' }}</span>
+</a>
